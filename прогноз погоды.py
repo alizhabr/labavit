3c380d3a404182bf24a78608ebe84f72fb0c6165
alizhabr <105541564+alizhabr@users.noreply.github.com>
2023-10-24 00:15:49 +0300

    Create прогноз погоды.py

---
+++ прогноз погоды.py
@@ -0,0 +1,28 @@
+import requests
+s_city = "Moscow,RU"
+appid = "d1c27022bc9f5912bbf514c6f90687dd"
+res = requests.get("http://api.openweathermap.org/data/2.5/weather",
+params={'q': s_city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+data = res.json()
+
+print("Город:", s_city)
+print("Погодные условия:", data['weather'][0]['description'])
+print("Температура:", data['main']['temp'])
+print("Минимальная температура:", data['main']['temp_min'])
+print("Максимальная температура", data['main']['temp_max'])
+print("скорость ветра", data['wind']['speed'])
+print("видимость", data['clouds']['all'])
+
+***********************
+#для недели
+res = requests.get("http://api.openweathermap.org/data/2.5/forecast",
+params={'q': s_city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+data = res.json()
+print("Прогноз погоды на неделю:")
+for i in data['list']:
+    print("Дата <", i['dt_txt'], "> \r\nТемпература <",
+    '{0:+3.0f}'.format(i['main']['temp']), "> \r\nПогодные условия <",
+    i['weather'][0]['description'], "> \r\nскорость ветра <",
+    i['wind']['speed'],">\r\nвидимость <",
+    i['clouds']['all'],">")
+print("____________________________")
