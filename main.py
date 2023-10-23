3ec911870e7aba9e6751bef176d02405aa053b47
alizhabr <105541564+alizhabr@users.noreply.github.com>
2023-10-23 21:59:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-a=int(input())
-b=int(input())
-c=int(input())
-d=b**2-4*a*c
-if d<0:
-    print("нет решения")
-if d==0:
-    print("корень: ", -b/(2*a))
-else:
-    print("1 корень: ", (-b+d**.5)/(2*a))
-    print("2 корень: ",(-b-d**.5)/(2*a))
+sides = [3, 2, 4, 7, 5, 12, 11, 13, 15, 16, 14, 14]
+sides = sorted(sides, reverse=True)
+smax = 0
+for i in range(len(sides)):
+    for j in range(i + 1, len(sides)):
+        for k in range(j + 1, len(sides)):
+            a = sides[i]
+            b = sides[j]
+            c = sides[k]
+            if a + b > c and a + c > b and b + c > a:
+                p = (a + b + c) / 2
+                s = (p * (p - a) * (p - b) * (p - c)) ** (1 / 2)
+                if s > smax:
+                    smax = s
+print("Максимальная площадь треугольника", smax)
